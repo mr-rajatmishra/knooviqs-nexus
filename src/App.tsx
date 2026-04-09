66da34b62e5a80d0d187285f3b70cb27bc04b4c4
RAJAT MISHRA <mrajat099@gmail.com>
2026-04-10 04:06:53 +0530

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Index from "./pages/Index.tsx";
-import NotFound from "./pages/NotFound.tsx";
 
 const queryClient = new QueryClient();
 
@@ -10,8 +9,7 @@ const App = () => (
     <BrowserRouter basename={import.meta.env.BASE_URL}>
       <Routes>
         <Route path="/" element={<Index />} />
-        {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-        <Route path="*" element={<NotFound />} />
+        <Route path="*" element={<Index />} />
       </Routes>
     </BrowserRouter>
   </QueryClientProvider>
