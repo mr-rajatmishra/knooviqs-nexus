78c4d98b483284ca0e7f52a8a8874c70c9daafcf
mr-rajatmishra <143219939+mr-rajatmishra@users.noreply.github.com>
2026-04-10 03:28:48 +0530

    Update base in vite.config.ts from '/' to '/knooviq-nexus/'

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,6 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
+import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  base: "/",
-  server: {
-    host: "::",
-    port: 8080,
-    hmr: {
-      overlay: false,
-    },
-  },
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-    dedupe: ["react", "react-dom", "react/jsx-runtime", "react/jsx-dev-runtime"],
-  },
-}));
+export default defineConfig({
+  base: '/knooviq-nexus/',
+  // other configurations...
+});
